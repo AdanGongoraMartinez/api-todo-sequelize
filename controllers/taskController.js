53c1e5d496e1468a2ba0c133f00c07bdb3faef5f
Adan Gongora <adangonmar@gmail.com>
2024-12-11 00:06:34 -0600

    Tasks module complete

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,9 +1,10 @@
-import pool from '../db.js';
+import Task from '../models/task.js';
+
 // Obtener todas las tareas
 export const getAllTasks = async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM tasks ORDER BY id ASC');
-        res.json(result.rows);
+        const result = await Task.findAll();
+        res.json(result);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Error en el servidor');
@@ -14,10 +15,8 @@ export const getAllTasks = async (req, res) => {
 export const getAllUserTasks = async (req, res) => {
     const { user_id } = req.params;
     try {
-        const result = await pool.query('SELECT * FROM tasks WHERE user_id=$1 ORDER BY id ASC',
-            [user_id]
-        );
-        res.json(result.rows);
+        const result = await Task.findAll({ where: { user_id } });
+        res.json(result);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Error en el servidor');
@@ -28,11 +27,8 @@ export const getAllUserTasks = async (req, res) => {
 export const getTaksById = async (req, res) => {
     const { id } = req.body;
     try {
-        const result = await pool.query(
-            'INSERT INTO tasks (id) VALUES ($1) RETURNING *',
-            [id]
-        );
-        res.json(result.rows[0]);
+        const result = await Task.findByPk(id);
+        res.json(result);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Error en el servidor');
@@ -42,12 +38,13 @@ export const getTaksById = async (req, res) => {
 // Crear una nueva tarea
 export const createTask = async (req, res) => {
     const { user_id, title } = req.body;
+    if (!user_id || !title) {
+        return res.status(400).send('Todos los campos son obligatorios');
+    }
+
     try {
-        const result = await pool.query(
-            'INSERT INTO tasks (user_id, title) VALUES ($1,$2) RETURNING *',
-            [user_id, title]
-        );
-        res.json(result.rows[0]);
+        const result = await Task.create({ user_id, title })
+        res.json(result);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Error en el servidor');
@@ -57,16 +54,14 @@ export const createTask = async (req, res) => {
 // Actualizar una tarea
 export const updateTask = async (req, res) => {
     const { id } = req.params;
-    const { completed } = req.body;
     try {
-        const result = await pool.query(
-            'UPDATE tasks SET completed = $1 WHERE id = $2 RETURNING *',
-            [completed, id]
-        );
-        if (result.rows.length === 0) {
+        const taskToUpdate = await Task.findByPk(id);
+        if (!taskToUpdate) {
             return res.status(404).send('Tarea no encontrada');
         }
-        res.json(result.rows[0]);
+        taskToUpdate.completed = !(taskToUpdate.completed)
+        await taskToUpdate.save()
+        res.json(taskToUpdate);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Error en el servidor');
@@ -76,12 +71,21 @@ export const updateTask = async (req, res) => {
 // Eliminar una tarea
 export const deleteTask = async (req, res) => {
     const { id } = req.params;
+
     try {
-        const result = await pool.query('DELETE FROM tasks WHERE id = $1 RETURNING *', [id]);
-        if (result.rows.length === 0) {
+        // Buscar la tarea por su ID
+        const taskToDelete = await Task.findByPk(id);
+
+        // Verificar si la tarea existe
+        if (!taskToDelete) {
             return res.status(404).send('Tarea no encontrada');
         }
-        res.json({ message: 'Tarea eliminada con éxito' });
+
+        // Eliminar la tarea
+        await taskToDelete.destroy();
+
+        // Responder con éxito
+        res.status(200).send('Tarea eliminada correctamente');
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Error en el servidor');
--- models/task.js
+++ models/task.js
@@ -8,7 +8,21 @@ const Task = sequelize.define("taks", {
         validate: {
             notEmpty: true, // Asegura que no sea una cadena vacía
         },
-    }
+    },
+    user_id: {
+        type: DataTypes.INTEGER, // Identificador del usuario
+        allowNull: false, // No permite valores nulos
+        references: {
+            model: "users", // Nombre de la tabla referenciada
+            key: "id", // Columna referenciada
+        },
+        onUpdate: "CASCADE", // Actualiza en cascada si el ID del usuario cambia
+        onDelete: "CASCADE", // Elimina la tarea si el usuario es eliminado
+    },
+    completed: {
+        type: DataTypes.BOOLEAN, // El atributo completed será un booleano
+        defaultValue: false, // Valor por defecto es 'false', indicando que la tarea no está completada
+    },
 }, {
     timestamps: true, // Incluye columnas createdAt y updatedAt
 });
